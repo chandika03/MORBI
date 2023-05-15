8f10f87467c039c116d650f8e9e46f9bbc0b3838
avipsajoshi <avipsa.joshi@gmail.com>
2023-05-15 21:25:51 +0545

    chat db

--- chat/chatmodule.php
+++ chat/chatmodule.php
@@ -1,7 +1,16 @@
 <?php 
   session_start();
+  if(!isset($_SESSION['user'])){
+    header("Location: /login.php");
+    exit();
+  }
   include('../dbconn.php');
   $toUser = $_GET['toId'];
+  $stmt = $pdo->prepare("INSERT INTO message (fromUser, toUser) VALUE (:fromuser,:touser)");
+  $stmt->bindParam(':fromuser', $_SESSION['user']);
+  $stmt->bindParam(':touser', $toUser);
+  $stmt->execute();
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,6 +20,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Message</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+  <link rel="stylesheet" href="user.css" />
 </head>
 <body>
   <div class="modal-dialog">
@@ -18,18 +28,11 @@
       <div class="modal_header">
         <h4></h4>
       </div>
-      <div class="modal_body">
-        <ol>
-        <?php 
-          $users = $pdo->query("SELECT * FROM users ");
-          $user_data = $users->fetchall(PDO::FETCH_ASSOC);
-          foreach($user_data as $user){
-            echo '<li>
-            <a href="chatmodule.php?user_id=' . $user['user_id'].'">'.$user["user_name"].'</a>
-            </li>';
-          }
-        ?>
-        </ol>
+      <div class="image-content">
+        <span class="overlay"></span>
+        <div class="card-image">
+          <img src="1.jpg" alt="" class="card-img" />
+        </div>
       </div>
     </div>
   </div>
@@ -44,33 +47,35 @@
         </div>
         <div class="typing-field">
             <div class="input-data">
-                <input id="data" type="text" placeholder="Type something here.." required>
+              <form action='message.php?toId=<?php echo $toUser;?>' method="post">
+                <input id="data" name="data" type="text" placeholder="Type something here.." required>
                 <button id="send-btn">Send</button>
+              </form>
             </div>
         </div>
     </div>
     <script>
-        $(document).ready(function(){
-            $("#send-btn").on("click", function(){
-                $value = $("#data").val();
-                $msg = '<div class="user-inbox inbox"><div class="msg-header"><p>'+ $value +'</p></div></div>';
-                $(".form").append($msg);
-                $("#data").val('');
+        // $(document).ready(function(){
+        //     $("#send-btn").on("click", function(){
+        //         $value = $("#data").val();
+        //         $msg = '<div class="user-inbox inbox"><div class="msg-header"><p>'+ $value +'</p></div></div>';
+        //         $(".form").append($msg);
+        //         $("#data").val('');
                 
-                // start ajax code
-                $.ajax({
-                    url: 'message.php',
-                    type: 'POST',
-                    data: 'text='+$value,
-                    success: function(result){
-                        $replay = '<div class="bot-inbox inbox"><div class="icon"><i class="fas fa-user"></i></div><div class="msg-header"><p>'+ result +'</p></div></div>';
-                        $(".form").append($replay);
-                        // when chat goes down the scroll bar automatically comes to the bottom
-                        $(".form").scrollTop($(".form")[0].scrollHeight);
-                    }
-                });
-            });
-        });
+        //         // start ajax code
+        //         $.ajax({
+        //             url: 'message.php',
+        //             type: 'POST',
+        //             data: 'text='+$value,
+        //             success: function(result){
+        //                 $replay = '<div class="bot-inbox inbox"><div class="icon"><i class="fas fa-user"></i></div><div class="msg-header"><p>'+ result +'</p></div></div>';
+        //                 $(".form").append($replay);
+        //                 // when chat goes down the scroll bar automatically comes to the bottom
+        //                 $(".form").scrollTop($(".form")[0].scrollHeight);
+        //             }
+        //         });
+        //     });
+        // });
     </script>
 </body>
 </html>
