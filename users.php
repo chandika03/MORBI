bc641dd4485958e86fe4f1c5377e16b9cfb4f8e3
Aayush Timalsina <xayush.dc@gmail.com>
2023-05-15 21:12:02 +0545

    Search function fixed

--- users.php
+++ users.php
@@ -13,22 +13,20 @@
     $i = 0;
     $j = 0;
     $searchValue;
-    @$user_id = $_GET[$i];
+  
     @$count = $_GET['count'];
+   
+    $searchValue = false;
 
     do{
-        if($user_id != null){
+        if($count != null){
+                @$user_id = $_GET[$i];
                 $stmt = $pdo->prepare("SELECT * FROM users WHERE user_id = :user_id");
                 $stmt ->bindParam(':user_id', $user_id);
                 $stmt ->execute();
             $value[$i] = $stmt->fetchAll(PDO::FETCH_ASSOC);
             $searchValue = true;
             }
-
-            
-          else{
-            $searchValue = false;
-          }
         $i++;
     }while($i<$count);
 ?>
@@ -45,48 +43,7 @@
     <link rel="stylesheet" href="user.css" />
   </head>
   <body>
-  <?php
-  
-        if($searchValue){
-        foreach($value as $item){?>
-        
-            <div class="slide-container swiper">
-      <div class="slide-content">
-        <div class="card-wrapper swiper-wrapper">
-          <div class="card swiper-slide">
-            <div class="image-content">
-              <span class="overlay"></span>
-
-              
-              <div class="card-image">
-                <img src="1.jpg" alt="" class="card-img" />
-              </div>
-            </div>
-            <div class="card-content">
-              <h2 class="name"><?php echo $item[0]['user_name'] ?></h2>
-              <p class="description">
-              <?php echo $item[0]['user_details'] ?>
-              </p>
-
-              <button class="button">View More</button>
-            </div>
-            
-          </div>
-            </div>
-          </div>
-        </div>
-      </div>
-     
-        <?php 
-        $j += 1;
-        if($j>=$count){
-          break;
-        }
-        }}
-        
-        else{ 
-    ?>
-    <header>
+  <header>
       <!-- nav bar -->
       <nav class="navbar">
         <div class="container-fluid">
@@ -112,7 +69,47 @@
         </div>
       </nav>
     </header>
+  <?php
+  
+        if($searchValue){
+        foreach($value as $item){?>
+        
+        
+      <div class="slide-container swiper">
+      <div class="slide-content">
+        <div class="card-wrapper swiper-wrapper">
+          <div class="card swiper-slide">
+            <div class="image-content">
+              <span class="overlay"></span>
 
+              
+              <div class="card-image">
+                <img src="1.jpg" alt="" class="card-img" />
+              </div>
+            </div>
+            <div class="card-content">
+              <h2 class="name"><?php echo $item[0]['user_name'] ?></h2>
+              <p class="description">
+              <?php echo $item[0]['user_details'] ?>
+              </p>
+              <button class="button">View More</button>
+            </div>
+            
+          </div>
+            </div>
+          </div>
+        </div>
+      </div>
+     
+        <?php 
+        $j += 1;
+        if($j>=$count){
+          break;
+        }
+        }}
+        
+        else{ 
+    ?>
     <!-- profile -->
     <?php
         foreach ($value as $item){
