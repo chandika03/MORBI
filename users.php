0bedf74581015f5b2376bf7efff1244c9bfe0175
Raisha58 <2058raisha@gmail.com>
2023-08-13 12:29:35 +0545

    removed swipper

--- users.php
+++ users.php
@@ -422,10 +422,10 @@ else{
         </div>     
       </div>
     </div>
-    <div class="swiper-button-next"></div>
+    <!-- <div class="swiper-button-next"></div>
     <div class="swiper-button-prev"></div>
     <div class="swiper-pagination">
-      </div>
+      </div> -->
       <?php } ?>
       
   </section>
